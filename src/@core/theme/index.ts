eb908b00580a0456603ebea9177b85cb1857d9a5
Hisham PP <mohammedhisham@kiebot.com>
2025-01-02 11:27:21 +0530

    Typography added

--- src/@core/theme/index.ts
+++ src/@core/theme/index.ts
@@ -1,9 +1,14 @@
 import { Theme, ThemeOptions } from "@mui/material";
 import palette from "./palette";
 import { Settings } from "@core/providers/SettingsProvider";
+import typography from "./typography";
 
 const themeOptions = (theme: Theme, settings: Settings): ThemeOptions => {
-  return { ...theme, palette: palette(theme, settings) };
+  return {
+    ...theme,
+    palette: palette(theme, settings),
+    typography: typography(theme),
+  };
 };
 
 export default themeOptions;
