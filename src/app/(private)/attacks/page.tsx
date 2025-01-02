a6f7c6c4e9e18e72a8e0e4bfeaa14e26fe0a863a
Hisham PP <mohammedhisham@kiebot.com>
2025-01-02 10:25:32 +0530

    attack and stages page added

---
+++ src/app/(private)/attacks/page.tsx
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+
+const Attacks = () => {
+  return <>Attacks</>;
+};
+
+export default memo(Attacks);
---
+++ src/app/(private)/stages/page.tsx
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+
+const Stages = () => {
+  return <>Stages</>;
+};
+
+export default memo(Stages);
