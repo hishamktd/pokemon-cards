56e299a8417d49b540236d23cc4cc6bbd42cacda
Hisham PP <mohammedhisham@kiebot.com>
2025-01-02 11:56:08 +0530

    added eslit config for import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,43 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["import"],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "@mui/**",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          newlinesBetween: "always",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
