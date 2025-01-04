defeea9daed124bd1cf8953f5ec2c0feeb102c7b
Hisham <hishammuhammed.in@gmail.com>
2025-01-04 06:07:25 +0530

    folder name changes

--- src/core/components/app-inputs/index.ts
+++ src/core/components/app-inputs/index.ts
@@ -1,4 +1,4 @@
-export { default as AppTextField } from './AppTextfield-temp';
-export { default as AppNumberField } from './AppNumberField-temp';
+export { default as AppTextField } from './AppTextField-te';
+export { default as AppNumberField } from './AppNumberField-te';
 
 export type { AppTextFieldProps } from './types';
