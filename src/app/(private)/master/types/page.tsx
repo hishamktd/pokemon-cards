1afd8bd5c2d33fc3947a294eb96c2535964470c3
Hisham <hishammuhammed.in@gmail.com>
2025-01-25 15:15:46 +0530

    Add types crud

--- src/app/(private)/master/types/page.tsx
+++ src/app/(private)/master/types/page.tsx
@@ -1,7 +1,84 @@
-import React, { memo } from 'react';
+import { GridColDef } from '@mui/x-data-grid';
+import React, { memo, useCallback, useMemo } from 'react';
+
+import ActionButton from '@/components/action-button';
+import Page from '@/components/page';
+import useTypesStore from '@/store/masters/types';
+import { DeleteItem } from '@/types';
+import { INITIAL_PAGE } from '@/utils/pagination';
+import { AppDataGrid } from '@core/components/app-table';
+import { PaginationSearchTitle } from '@core/components/app-title';
+import useQuery from '@core/hooks/use-query';
 
 const Types = () => {
-  return <>Types</>;
+  const { totalCount, entities } = useTypesStore();
+
+  const [page, setPage] = useQuery('page', INITIAL_PAGE);
+  const [query, setQuery] = useQuery('query', '');
+  const [isOpen, setIsOpen] = useQuery('drawer', false);
+  const [editId, setEditId] = useQuery('id', 0);
+  const [itemToDelete, setItemToDelete] = useQuery<DeleteItem>(
+    'itemToDelete',
+    null,
+  );
+
+  const toggleDrawer = useCallback(() => {
+    setIsOpen((prev) => !prev);
+  }, [setIsOpen]);
+
+  const handleDelete = useCallback(
+    (id: number, name?: string) => {
+      setItemToDelete({ id, name });
+    },
+    [setItemToDelete],
+  );
+
+  const handleOnEdit = useCallback(
+    (id: number) => {
+      setEditId(id);
+      toggleDrawer();
+    },
+    [setEditId, toggleDrawer],
+  );
+
+  const columns = useMemo<GridColDef[]>(
+    () => [
+      { field: 'id', headerName: 'ID' },
+      { field: 'name', headerName: 'Name' },
+      { field: 'totalCards', headerName: 'Total Cards' },
+      {
+        field: 'actions',
+        headerName: 'Actions',
+        renderCell: ({ row }) => (
+          <>
+            <ActionButton for="update" id={row?.id} onClick={handleOnEdit} />
+            <ActionButton
+              for="delete"
+              id={row?.id}
+              name={row?.name}
+              onClick={handleDelete}
+            />
+          </>
+        ),
+      },
+    ],
+    [handleDelete, handleOnEdit],
+  );
+
+  return (
+    <Page>
+      <PaginationSearchTitle
+        title="Packs"
+        variant="small"
+        buttonGroupProps={{
+          containedButtonProps: { label: 'Create', onClick: toggleDrawer },
+        }}
+        paginationProps={{ totalCount, onPageChange: setPage, page }}
+        searchProps={{ query, onChange: setQuery }}
+      />
+      <AppDataGrid rows={entities} columns={columns} />
+    </Page>
+  );
 };
 
 export default memo(Types);
