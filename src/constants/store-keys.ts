8b03c1d78609e7f35541048912df8b9b502b39c3
Hisham <hishammuhammed.in@gmail.com>
2025-01-25 18:34:42 +0530

    Add axios for API requests and implement request/response interceptors

--- src/constants/store-keys.ts
+++ src/constants/store-keys.ts
@@ -1,3 +1,4 @@
 export const LOCAL_STORAGE_KEYS = {
   SETTINGS: 'app-settings',
+  TOKEN: 'app-token',
 };
---
+++ src/lib/interceptor/api.ts
@@ -0,0 +1,36 @@
+import axios, {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+
+import { LOCAL_STORAGE_KEYS } from '@/constants/store-keys';
+import { Any } from '@/types';
+
+const { TOKEN } = LOCAL_STORAGE_KEYS;
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  timeout: 5000,
+});
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem(TOKEN);
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error: Any) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error: AxiosError) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
