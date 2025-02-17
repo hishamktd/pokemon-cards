aadb22f8411c3ad34359ea335347d9d64aefa474
Hisham <hishammuhammed.in@gmail.com>
2025-02-17 09:30:27 +0530

    Rename thumbnail to image in expansion default values

--- src/constants/masters/expansion.ts
+++ src/constants/masters/expansion.ts
@@ -3,5 +3,5 @@ import { ExpansionForm } from '@/types/masters/expansion';
 export const expansionDefaultValues: ExpansionForm = {
   name: '',
   totalCards: '',
-  thumbnail: null,
+  image: null,
 };
