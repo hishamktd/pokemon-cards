d7d6ec8d5f4802a0d481495341d53132829ada65
Hisham <hishammuhammed.in@gmail.com>
2025-03-01 11:23:53 +0530

    feat: Wrap ColorChip component in Box for improved layout and styling

--- src/components/chips/ColorChip.tsx
+++ src/components/chips/ColorChip.tsx
@@ -1,16 +1,25 @@
-import { Paper, Tooltip } from '@mui/material';
+import { Box, Paper, Tooltip } from '@mui/material';
 import React, { FC, memo } from 'react';
 
 type Props = {
   color?: string;
 };
 
+const getBoxStyle = () => ({
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%',
+});
+
 const ColorChip: FC<Props> = ({ color }) => {
   if (!color) return <>-</>;
   return (
-    <Tooltip title={color}>
-      <Paper sx={{ width: 24, height: 24, backgroundColor: color }}></Paper>
-    </Tooltip>
+    <Box sx={getBoxStyle}>
+      <Tooltip title={color}>
+        <Paper sx={{ width: 24, height: 24, backgroundColor: color }}></Paper>
+      </Tooltip>
+    </Box>
   );
 };
 
