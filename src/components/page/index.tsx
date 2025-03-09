08c51f37d8b1811ff7e33325e2fbb9a4732cff0d
Hisham <hishammuhammed.in@gmail.com>
2025-03-09 10:12:09 +0530

    refactor: Simplify Stack component props in Page component

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -7,13 +7,7 @@ type Props = StackProps & {
 
 const Page: React.FC<Props> = ({ children, ...props }) => {
   return (
-    <Stack
-      gap={2}
-      width="100%"
-      height="calc(100vh - 80px)"
-      overflow="auto"
-      {...props}
-    >
+    <Stack gap={2} width="100%" height="calc(100vh - 80px)" {...props}>
       {children}
     </Stack>
   );
