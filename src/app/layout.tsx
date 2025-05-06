45a1e8a96c7f725e0b32594e01070a7ddb72d04b
Hisham <hishammuhammed.in@gmail.com>
2025-05-06 20:34:50 +0530

    Add lazy-loaded script for react-scan in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 
 import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 
 import { ReduxProvider } from '@core/providers/ReduxProvider';
 import { SettingsProvider } from '@core/providers/SettingsProvider';
@@ -18,6 +19,11 @@ export default async function RootLayout({
   return (
     <html>
       <body>
+        <Script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+          strategy="lazyOnload"
+        />
         <ReduxProvider>
           <AppRouterCacheProvider>
             <SettingsProvider>{children}</SettingsProvider>
